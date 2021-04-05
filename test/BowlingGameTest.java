db4d4612efc62412de19803bed3d96a84330a070
woasidh <woasidh1@naver.com>
2021-04-05 23:25:04 +0900

    the second test(updated)

--- test/BowlingGameTest.java
+++ test/BowlingGameTest.java
@@ -1,7 +1,6 @@
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.*;
-import org.junit.jupiter.api.Test;
 
 public class BowlingGameTest {
 	private Game g;
@@ -13,7 +12,6 @@ public class BowlingGameTest {
 	
 	@Test
 	public void testGutterGame() {
-		Game g = new Game();
 		for (int i = 0; i < 20; i++)
 			g.roll(0);
 		assertEquals(0, g.score());
@@ -21,7 +19,6 @@ public class BowlingGameTest {
 
 	@Test
 	public void testAllOnes() {
-		Game g = new Game();
 		for (int i = 0; i < 20; i++)
 			g.roll(1);
 		assertEquals(20, g.score());
