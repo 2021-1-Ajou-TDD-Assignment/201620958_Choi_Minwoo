fb1366af6d941305a12376c19b4c8422893874d7
woasidh <woasidh1@naver.com>
2021-04-05 23:33:00 +0900

    the fourth test

--- test/BowlingGameTest.java
+++ test/BowlingGameTest.java
@@ -27,4 +27,14 @@ public class BowlingGameTest {
 		rollMany(20, 1);
 		assertEquals(20, g.score());
 	}
+	
+	@Ignore("until we get design right")
+	@Test
+	public void testOneSpare() {
+		g.roll(5);
+		g.roll(5); //spare
+		g.roll(3);
+		rollMany(17, 0);
+		assertEquals(16, g.score());
+	}
 }
