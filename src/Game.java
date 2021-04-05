ded592da5e8d89a46188a4de37a1b74b9620c292
woasidh <woasidh1@naver.com>
2021-04-05 23:45:54 +0900

    the eighth test

--- src/Game.java
+++ src/Game.java
@@ -11,7 +11,11 @@ public class Game {
 		int score = 0;
 		int frameIndex = 0;
 		for (int frame = 0; frame < 10; frame++) {
-			if (isSpare(frameIndex)) {
+			if(isStrike(frameIndex)) {
+				score+=10+strikeBonus(frameIndex);
+				frameIndex++;
+			}
+			else if (isSpare(frameIndex)) {
 				score += 10 + rolls[frameIndex + 2];
 				frameIndex += 2;
 			} else {
@@ -24,5 +28,13 @@ public class Game {
 	
 	private boolean isSpare(int frameIndex) {
 		return rolls[frameIndex] + rolls[frameIndex+1]==10;
+	}
+	
+	private boolean isStrike(int frameIndex) {
+		return rolls[frameIndex] == 10;
+	}
+	
+	private int strikeBonus(int frameIndex) {
+		return rolls[frameIndex+1] + rolls[frameIndex+2];
 	}
 }
